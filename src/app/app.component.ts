10619ce348eb07a8ecd0cd5858ee2500c7c505b5
ChetanJangid <jangid.chetan5@gmail.com>
2024-01-20 16:07:53 +0530

    added audio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'static-angular-app-on-render';
+  audio: HTMLAudioElement | null = null;
+
+  constructor() {
+    document.addEventListener('DOMContentLoaded', () => {
+      this.audio = document.getElementById('myAudio') as HTMLAudioElement;
+      if (this.audio) {
+        this.audio.play();
+      }
+    });
+  }
+
+  
 }
